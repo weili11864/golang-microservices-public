af68e4134f6fa2ba8332c7d38a152fd9a22e3d5d
Allen Sanders <asanders@gamuttechnologysvcs.com>
2021-12-09 18:00:02 -0500

    updates

---
+++ demos/inventory/internal/inventory/inventory.go
@@ -0,0 +1,15 @@
+package inventory
+
+// This should actually be pulled into a separate package
+// since used from multiple locations
+type Inventory struct {
+	ID            uint   `json:"id"`
+	ProductNumber string `json:"productNumber"`
+	Quantity      int    `json:"quantity"`
+}
+
+type InventoryDatastore interface {
+	CreateInventory(inventory *Inventory) error
+	UpdateInventory(id string, inventory Inventory) error
+	GetInventory(productNumber string) (Inventory, error)
+}
